68a0067d4f3e8dcdf290b14228f1ae252998d986
Jhefferson Muzy <jheffersonmuzy46@gmail.com>
2024-02-15 16:58:47 -0300

    Update: Modified several files and added a favicon

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,26 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <the-header></the-header>
+  <main>
+    <router-view></router-view>
+  </main>
+  <aside>
+    <social-media></social-media>
+  </aside>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/ui/TheHeader.vue'
+import SocialMedia from './components/ui/SocialMedia.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    TheHeader,
+    SocialMedia
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap');
+body {
+  position: relative;
+  height: 100vh;
+  font-family: 'Outfit', sans-serif;
+  background-image: url('../src/assets/black_bg.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+  color: #fff;
+}
+
+/* global classes */
+.text-color {
+  color: #fff !important;
+}
+.body-text{
+  font-size: 14px;
+  font-weight: 400;
+}
+.title{
+  font-size: 52px;
+  font-weight: 900;
+  text-transform: capitalize;
+}
+.subtitle{
+  font-size: 33.5px;
+  font-weight: 100;
+  text-transform: lowercase;
+}
+.gradient-text, .gradient-text-hover:hover{
+  color: transparent;
+  background: hsla(285, 80%, 72%, 1);
+  background: linear-gradient(0deg, hsla(285, 80%, 72%, 1) 0%, hsla(219, 85%, 68%, 1) 50%, hsla(258, 63%, 60%, 1) 100%);
+  background: -moz-linear-gradient(0deg, hsla(285, 80%, 72%, 1) 0%, hsla(219, 85%, 68%, 1) 50%, hsla(258, 63%, 60%, 1) 100%);
+  background: -webkit-linear-gradient(0deg, hsla(285, 80%, 72%, 1) 0%, hsla(219, 85%, 68%, 1) 50%, hsla(258, 63%, 60%, 1) 100%);
+  -webkit-background-clip: text;
+  background-clip: text;
+}
+.gradient-text-hover{
+  transition: .5s ease;
+}
+.btn:focus-visible, .btn:focus{
+  box-shadow: none;
+  border: none;
+  outline: none;
 }
 </style>
