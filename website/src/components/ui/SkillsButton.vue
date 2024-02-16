c08164d4fd7857c5f35e6afd70672e2789119836
Jhefferson Muzy <jheffersonmuzy46@gmail.com>
2024-02-16 17:32:09 -0300

    feat: Created a UI component called 'SkillsButton'.

---
+++ website/src/components/ui/SkillsButton.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="box">
+      <span class="text text-color">{{ skillName }}</span>
+    </div>
+</template>
+<script>
+export default {
+  props: ['skillName']
+}
+</script>
+
+<style scoped>
+.box{
+  display: grid;
+  place-content: center;
+  padding: 6px 18px;
+  min-width: 70px;
+  min-height: 35px;
+  border-radius: 25px;
+  background-color: #1e1e1e;
+  transition: .6s ease;
+}
+.box:hover{
+  background: #7F59D9;
+}
+.text{
+  font-size: 14px;
+  letter-spacing: 0.032px;
+  text-transform: uppercase;
+  font-weight: 600;
+}
+</style>
